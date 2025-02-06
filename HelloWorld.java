cd1d363723aade4e9199a0a89b6e026ad35304d8
Hiram Pina Valdes <hiram.p@gmail.com>
2025-02-05 21:47:38 -0600

    Testing hashcodes for two variables pointing to the same object

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) { 
         System.out.println("Hello world!");
+        //System.out.println(args[0]);
+
+
+        String string1 = "Hello";
+        String string2 = "Hello";
+
+        System.out.println(string1 == string2);
+        System.out.println(string1.hashCode());
+        System.out.println(string2.hashCode());
     }
 }
