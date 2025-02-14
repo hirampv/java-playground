c1871127d7fcb0d86a617b1d01836ff2490be4ae
Hiram Pina Valdes <hiram.p@gmail.com>
2025-02-14 13:49:32 -0600

    Create Triangle class to test inheritance from Shape parent class

---
+++ Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle extends Shape {
+    public Triangle() {
+        super(3);
+    }
+
+
+    public static void main(String[] args) {
+        Triangle myTriangle = new Triangle();
+        System.out.println(myTriangle);
+    }
+}
